9505adb2bc029506e5a79d27b0859426e31fc836
vanshita Roopchandani <vanshitaroopchandani7@gmail.com>
2023-01-20 04:34:17 +0530

    change db link

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
-const mongoURI = "mongodb+srv://vanshita:waheguru@cluster0.v2epw4n.mongodb.net/test"
+const mongoURI = "mongodb+srv://vanshita:waheguru@cluster0.v2epw4n.mongodb.net/inotebook"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
