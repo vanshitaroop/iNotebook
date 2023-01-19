256dc1754105bd975baecc3bb88adef16fa36157
vanshita Roopchandani <vanshitaroopchandani7@gmail.com>
2023-01-20 04:18:37 +0530

    fot test again

--- src/compnents/Signup.js
+++ src/compnents/Signup.js
@@ -14,7 +14,7 @@ export const Signup = (props) => {
     e.preventDefault();
     const { name, email, password } = credantials;
 
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("https://i-notebook-two.vercel.app/api/auth/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
